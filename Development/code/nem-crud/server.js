def9cf952e452827d6eb343077b865f00fce6ae1
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-03-13 09:47:06 -0500

    finished server.js

--- Development/code/nem-crud/server.js
+++ Development/code/nem-crud/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
 const path = require('path');
+const { ppid } = require('process');
 
 
 const app = express();
@@ -18,6 +19,13 @@ app.use(bodyparser.urlencoded({extended:true}))
 
 // set view engine
 app.set("view engine", "ejs")
+
+
+// load assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")))
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")))
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")))
+
 
 app.get('/', (req, res) => {
     res.send("Crud Application");

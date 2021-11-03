7390daecda2aa99a0a9ef650a1393f267907226f
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-11-03 11:44:26 -0400

    set up the css page for todo.js. set up the form in todo.js. moving towards creating db

--- Development/code/todos/src/App.js
+++ Development/code/todos/src/App.js
@@ -5,6 +5,7 @@ import Todo from "./components/Todo";
 function App() {
   return (
     <div className="App">
+      <h1 className="todoHdr">todos</h1>
       <Todo />
     </div>
   );
--- Development/code/todos/src/components/Todo.js
+++ Development/code/todos/src/components/Todo.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/Todo.css";
 
 const Todo = () => {
-  return <div>Create a task here</div>;
+  const [task, setTask] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = () => {
+    console.log("submitted");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Task:
+          <input
+            type="text"
+            value={task}
+            placeholder="What needs to be done?"
+            onChange={(e) => setTask(e.target.value)}
+          />
+        </label>
+        <br />
+        <br />
+        <label>
+          Due Date:
+          <input
+            type="text"
+            value={dueDate}
+            onChange={(e) => setDueDate(e.target.value)}
+          />
+        </label>
+        <br />
+        <br />
+        <label>
+          Category:
+          <input
+            type="text"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+        </label>
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default Todo;
